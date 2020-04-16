be0fa4a3e5d2086ea074b4b60eecbe20052070a4
Faizal Ardian Putra <faizal@trutrip.co>
2020-04-17 00:51:45 +0700

    naikin build

---
+++ build/precache-manifest.a0c61bfd4f7d18b60b99ed667310032b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9992448f98f96b564dfaa9d9917219ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c542dfb90d0d30849819",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "1a44cc036635e9f97fb7",
+    "url": "/static/js/2.0aa3222c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0aa3222c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c542dfb90d0d30849819",
+    "url": "/static/js/main.a5680da9.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  }
+]);
