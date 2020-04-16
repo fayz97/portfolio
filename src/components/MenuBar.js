96f0af376bc2c101fa34fa5cb4d81261570470b5
Faizal Ardian Putra <faizal@trutrip.co>
2020-04-16 22:24:52 +0700

    app bar

---
+++ src/components/MenuBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import { Toolbar, Typography, Grid } from '@material-ui/core';
+import '../styles/components/MenuBar.css';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+    app_bar: {
+        background: '#000000',
+    },
+    title: {
+        // padding: '20px',
+        textAlign: 'center',
+        letterSpacing: '10px',
+    }
+}));
+
+export default function MenuBar() {
+    
+    const classes = useStyles();
+
+    // Render
+    return (
+        <div className="MenuBar">
+            <AppBar position="static" className={classes.app_bar}>
+                <Toolbar>
+                    <Grid container>
+                        <Grid item xs>
+                            <Typography variant="h6" className={classes.title}>
+                                PORTFOLIO
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
